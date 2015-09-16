5b619e44b6ce960e0b6e4450a5e10365e12b1b4d
awahid101 <awahid@gmail.com>
2015-09-16 20:28:59 +1200

    added documentation for controller

--- src/Mist/Core/Controller/Controller.php
+++ src/Mist/Core/Controller/Controller.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Mist\Core\Controller;
 
 use Mist\Core\Template\View;
@@ -11,41 +12,71 @@ use Mist\Core\Template\View;
  * @date June 15th, 2015
  */
 
-abstract class Controller
-{
+abstract class Controller {
+
+    /**
+     * holds the controller name 
+     * @var string 
+     */
     protected $controller;
+    
+    /**
+     * holds the action name, by default this is set to index
+     * @var type 
+     */
     protected $action;
+    
+    /**
+     * holds the view name
+     * @var type 
+     */
     protected $view;
-            
- /**
-  * constructor
-  * 
-  * This is a main controller class that needs to be extended by all application controllers.
-  * 
-  * @param string $controller we must have crontroller a name. 
-  * @param string $action by default the action is index function.
-  * @param array $params list of parameters that will be passed to view class and are accessible in view.
-  * 
-  * @return void
-  */
-    function __construct($controller, $action = 'index', $params = array()) 
-    {    
+
+    /**
+     * constructor
+     * 
+     * This is a main controller class that needs to be extended by all application controllers.
+     * 
+     * @param string $controller we must have crontroller a name. 
+     * @param string $action by default the action is index function.
+     * @param array $params list of parameters that will be passed to view class and are accessible in view.
+     * 
+     * @return void
+     */
+    function __construct($controller, $action = 'index', $params = array()) {
         $this->controller = $controller;
-        $this->action = $action;        
+        $this->action = $action;
         $this->view = new View($controller, $action);
         $this->setParams('params', $params);
     }
- 
-    protected function setParams($name,$value) 
-    {
+
+    /**
+     * setParams
+     * 
+     * Sets the parameters of the view
+     * 
+     * @param string $name key value must be string because this will be the variable name
+     * @param mixture $value can have multiple data types
+     * 
+     * @return void
+     */
+    protected function setParams($name, $value) {
         $this->view->set($name, $value);
     }
-     
-    protected function setLayout($layoutName) 
-    {
+
+    /**
+     * setLayout
+     * 
+     * sets the Layout of the application
+     * 
+     * @param type $layoutName
+     * 
+     * @return  void
+     */
+    protected function setLayout($layoutName) {
         $this->view->setLayout($layoutName);
     }
-    
+
     /**
      * setView
      * 
@@ -54,12 +85,10 @@ abstract class Controller
      * @param string $viewName
      * @return void
      */
-    
-    protected function setView($viewName) 
-    {
+    protected function setView($viewName) {
         $this->view->setView($viewName);
     }
-    
+
     /**
      * render
      * 
@@ -67,8 +96,8 @@ abstract class Controller
      * Question Time: What if I use desctructor to render the view?
      * @return void
      */
-    protected function render() 
-    {
-            $this->view->render();
+    protected function render() {
+        $this->view->render();
     }
+
 }
