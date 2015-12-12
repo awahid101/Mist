13df585eb73a7790f1895869745fe1b87ea9a40c
awahid101 <awahid@gmail.com>
2015-12-13 11:54:56 +1300

    added documentation

--- app/Controller/WelcomeController.php
+++ app/Controller/WelcomeController.php
@@ -10,7 +10,7 @@
 use Mist\Core\Controller\Controller;
 
 /**
- * Sample Welcome Controller for the mist frame work
+ * Sample Welcome Controller for the mist framework
  */
 
 class WelcomeController extends Controller
